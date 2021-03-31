1a8ff7509c3c781175b0109cdb27c9f47ab60331
Jonathan Arciaga <jonarciaga@gmail.com>
2021-03-31 16:53:19 -0500

    Split video player from mainwindow

--- WPFVideoPlayer/MainWindow.xaml.cs
+++ WPFVideoPlayer/MainWindow.xaml.cs
@@ -12,17 +12,5 @@ namespace WPFVideoPlayer
         {
             InitializeComponent();
         }
-
-        private void VideoTime_Slider_DragStarted(object sender, DragStartedEventArgs e)
-        {
-            MainWindowViewModel mwvm = (MainWindowViewModel)DataContext;
-            mwvm.Pause();
-        }
-
-        private void VideoTime_Slider_DragCompleted(object sender, DragCompletedEventArgs e)
-        {
-            MainWindowViewModel mwvm = (MainWindowViewModel)DataContext;
-            mwvm.Play();
-        }
     }
 }
--- WPFVideoPlayer/MainWindowViewModel.cs
+++ WPFVideoPlayer/MainWindowViewModel.cs
@@ -1,204 +1,60 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Threading;
+﻿using System.Windows.Input;
 
 using WPFVideoPlayer.Helpers;
-using WPFVideoPlayer.Models;
+using WPFVideoPlayer.ViewModels;
 
 namespace WPFVideoPlayer
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        /// <summary>
-        /// C:\Users\jarciaga\Downloads\EOIS Production Fixture Demo Videos
-        /// </summary>
         #region " Constructor " 
 
         public MainWindowViewModel()
         {
-            VolumeLevel = 1;
-            VideoStatusText = "Video Not Loaded\nPlease Select a Video";
-            VideoTimeRemainingText = "00:00 / 00:00";
-            _timer = new DispatcherTimer();
-            VideoModelCollection = new ObservableCollection<VideoModel>();
-
-            GetVideoCollectionFromSource();
         }
 
         #endregion
 
         #region " Properties "
 
-        private DispatcherTimer _timer;
-
-        private MediaElement _mediaElementObject;
-        public MediaElement MediaElementObject
+        private object _runningViewModel;
+        public object RunningViewModel
         {
-            get { return _mediaElementObject; }
+            get { return _runningViewModel; }
             set 
             { 
-                _mediaElementObject = value;
-                OnPropertyChanged("MediaElementObject");
+                _runningViewModel = value;
+                OnPropertyChanged("RunningViewModel");
             }
         }
 
-        private string _mediaLocation;
-        public string MediaLocation
+        private string _buttonContent = "Open Video Player";
+        public string ButtonContent
         {
-            get { return _mediaLocation; }
+            get { return _buttonContent; }
             set 
             { 
-                _mediaLocation = value;
-                OnPropertyChanged("MediaLocation");
+                _buttonContent = value;
+                OnPropertyChanged("ButtonContent");
             }
         }
 
-        private double _volumeLevel;
-        public double VolumeLevel
+        private bool _isVideoPlayerOpen;
+        public bool IsVideoPlayerOpen
         {
-            get { return _volumeLevel; }
-            set 
-            { 
-                _volumeLevel = value;
-
-                if (MediaElementObject != null)
-                {
-                    MediaElementObject.Volume = VolumeLevel;
-                }
-
-                OnPropertyChanged("VolumeLevel");
-            }
-        }
-
-        private bool _isMuted;
-        public bool IsMuted
-        {
-            get { return _isMuted; }
+            get { return _isVideoPlayerOpen; }
             set
-            {
-                _isMuted = value;
-
-                if (MediaElementObject != null)
-                {
-                    MediaElementObject.IsMuted = IsMuted;
-                }
-
-                OnPropertyChanged("IsMuted");
-            }
-        }
-
-        private double _sliderValue;
-        public double SliderValue
-        {
-            get { return _sliderValue; }
-            set 
-            {
-                _sliderValue = value;
-
-                if (MediaElementObject != null && MediaElementObject.HasVideo)
-                {
-                    MediaElementObject.Position = TimeSpan.FromSeconds(_sliderValue);
-                }
-
-                OnPropertyChanged("SliderValue");
-            }
-        }
-
-        private double _sliderMaximum;
-        public double SliderMaximum
-        {
-            get { return _sliderMaximum; }
-            set 
             { 
-                _sliderMaximum = value;
-                OnPropertyChanged("SliderMaximum");
-            }
-        }
+                _isVideoPlayerOpen = value;
 
-        private bool _isVideoPaused;
-        public bool IsVideoPaused
-        {
-            get { return _isVideoPaused; }
-            set
-            {
-                _isVideoPaused = value;
-                OnPropertyChanged("IsVideoPaused");
-            }
-        }
-
-        private bool _isMediaLoaded;
-        public bool IsMediaLoaded
-        {
-            get { return _isMediaLoaded; }
-            set 
-            { 
-                _isMediaLoaded = value;
-
-                if (_isMediaLoaded)
+                if (_isVideoPlayerOpen)
                 {
-                    VideoStatusText = "Press Play to Start";
+                    ButtonContent = "Close Video Player";
                 }
                 else
                 {
-                    VideoStatusText = "Video Not Loaded\nPlease Select a Video";
+                    ButtonContent = "Open Video Player";
                 }
-
-                OnPropertyChanged("IsMediaLoaded");
-            }
-        }
-
-        private string _videoStatusText;
-        public string VideoStatusText
-        {
-            get { return _videoStatusText; }
-            set
-            {
-                _videoStatusText = value;
-                OnPropertyChanged("VideoStatusText");
-            }
-        }
-
-        private string _videoTimeRemainingText;
-        public string VideoTimeRemainingText
-        {
-            get { return _videoTimeRemainingText; }
-            set
-            {
-                _videoTimeRemainingText = value;
-                OnPropertyChanged("VideoTimeRemainingText");
-            }
-        }
-
-        private ObservableCollection<VideoModel> _videoModelCollection;
-        public ObservableCollection<VideoModel> VideoModelCollection
-        {
-            get { return _videoModelCollection; }
-            set 
-            {
-                _videoModelCollection = value;
-                OnPropertyChanged("VideoModelCollection");
-            }
-        }
-
-        private VideoModel _selectedVideo;
-        public VideoModel SelectedVideo
-        {
-            get 
-            {
-                if (_selectedVideo != null)
-                {
-                    LoadAndPlayMediaElement(_selectedVideo);
-                }
-
-                return _selectedVideo; 
-            }
-            set 
-            { 
-                _selectedVideo = value;
-                OnPropertyChanged("SelectedVideo");
             }
         }
 
@@ -206,126 +62,28 @@ namespace WPFVideoPlayer
 
         #region " Methods "
 
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            try
-            {
-                if ((MediaElementObject.Source != null) && (MediaElementObject.NaturalDuration.HasTimeSpan))
-                {
-                    SliderMaximum = MediaElementObject.NaturalDuration.TimeSpan.TotalSeconds;
-                    SliderValue = MediaElementObject.Position.TotalSeconds;
-                    VideoTimeRemainingText = MediaElementObject.Position.ToString(@"mm\:ss") + " / " + MediaElementObject.NaturalDuration.TimeSpan.ToString(@"mm\:ss");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Timer_Tick() Exception: " + ex);
-            }
-        }
-
-        private void GetVideoCollectionFromSource()
-        {
-            try
-            {
-                foreach (string item in Directory.GetFiles(Properties.Settings.Default.VideosSourceLocation, "*", SearchOption.AllDirectories))
-                {
-                    if (item.Contains(".mp4") || item.Contains(".mov") || item.Contains(".wmv") || item.Contains(".avi"))
-                    {
-                        VideoModelCollection.Add(new VideoModel(item));
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("GetVideoCollectionFromSource() Exception: " + ex);
-            }
-        }
-
-        private void LoadAndPlayMediaElement(VideoModel _videoModel)
-        {
-            try
-            {
-                MediaLocation = _videoModel.Location;
-
-                MediaElementObject = new MediaElement
-                {
-                    Source = new Uri(_videoModel.Location),
-                    LoadedBehavior = MediaState.Manual,
-                    UnloadedBehavior = MediaState.Close
-                };
-
-                IsMediaLoaded = true;
-
-                Play();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("LoadAndPlayMediaElement() Exception: " + ex);
-            }
-        }
-
         #endregion
 
         #region " Command Methods "
 
-        public void Play()
+        private void ToggleVideoPlayer()
         {
-            try
+            IsVideoPlayerOpen = !IsVideoPlayerOpen;
+
+            if (IsVideoPlayerOpen)
             {
-                if (MediaElementObject != null)
+                Mouse.OverrideCursor = Cursors.Wait;
+
+                RunningViewModel = new VideoPlayerViewModel();
+
+                Mouse.OverrideCursor = Cursors.Arrow;
+            }
+            else
+            {
+                if (RunningViewModel != null)
                 {
-                    MediaElementObject.Play();
-
-                    _timer.Interval = TimeSpan.FromSeconds(1);
-                    _timer.Tick += Timer_Tick;
-                    _timer.Start();
-
-                    IsVideoPaused = false;
+                    RunningViewModel = null;
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Play() Exception: " + ex);
-            }
-        }
-
-        public void Pause()
-        {
-            try
-            {
-                if (MediaElementObject != null)
-                {
-                    MediaElementObject.Pause();
-                    IsVideoPaused = true;
-                }
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Pause() Exception: " + ex);
-            }
-        }
-
-        public void Stop()
-        {
-            try
-            {
-                if (MediaElementObject != null)
-                {
-                    MediaElementObject.Stop();
-                    MediaElementObject = null;
-
-                    IsMediaLoaded = false;
-                    IsVideoPaused = false;
-
-                    _timer.Stop();
-                    _timer = new DispatcherTimer();
-                    VideoTimeRemainingText = "00:00 / 00:00";
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Stop() Exception: " + ex);
             }
         }
 
@@ -333,45 +91,17 @@ namespace WPFVideoPlayer
 
         #region " Commands "
 
-        private ICommand _playCommand;
-        public ICommand PlayCommand
+        private ICommand _toggleVideoPlayerCommand;
+        public ICommand ToggleVideoPlayerCommand
         {
             get
             {
-                if (_playCommand == null)
+                if (_toggleVideoPlayerCommand == null)
                 {
-                    _playCommand = new RelayCommand(P => true, p => Play());
+                    _toggleVideoPlayerCommand = new RelayCommand(P => true, p => ToggleVideoPlayer());
                 }
 
-                return _playCommand;
-            }
-        }
-
-        private ICommand _pauseCommand;
-        public ICommand PauseCommand
-        {
-            get
-            {
-                if (_pauseCommand == null)
-                {
-                    _pauseCommand = new RelayCommand(P => true, p => Pause());
-                }
-
-                return _pauseCommand;
-            }
-        }
-
-        private ICommand _stopCommand;
-        public ICommand StopCommand
-        {
-            get
-            {
-                if (_stopCommand == null)
-                {
-                    _stopCommand = new RelayCommand(P => true, p => Stop());
-                }
-
-                return _stopCommand;
+                return _toggleVideoPlayerCommand;
             }
         }
 
